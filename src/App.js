59bb11329fa056e5a670087223eadac0fa0f85f7
Pr0kop <bartek.prokopczuk@gmail.com>
2022-05-23 21:48:13 +0200

    Intern Task 1,2,3,4. without 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
 import Dot from "./Dot";
 
 function App() {
-    return (
-        <div className="dots-wrapper">
-            {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="dots-wrapper">
+      {new Array(20).fill("#000000").map((_, i) => (
+        <Dot key={i} id={i} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/Dot.js
+++ src/Dot.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './Dot.css';
+import React, { useState } from "react";
+import "./Dot.css";
 
-const Dot = () => {
-    // TODO: put your code here
-    return (
-        <div className="dot"/>
-    );
+const Dot = ({ id }) => {
+  // TODO: put your code here
+  const [click, setClick] = useState(false);
+  const [color, setColor] = useState("#000");
+  const generateColor = () => {
+    setColor(Math.random().toString(16).substr(-6));
+  };
+
+  return (
+    <div
+      className="dot"
+      style={{
+        backgroundColor: "#" + color,
+      }}
+      onClick={() => {
+        setClick(!click);
+        generateColor();
+      }}
+    >
+      {click && id}
+    </div>
+  );
+  // function click() {
+  //   console.log("here");
+  // }
+  // console.log(element);
+  // // console.log(window);
+
+  // // click();
+  // return element;
 };
-
 export default Dot;
